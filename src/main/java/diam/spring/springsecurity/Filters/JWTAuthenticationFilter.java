1acc2aa06fe1543c8f3a5cf93ac5d85f48c070a7
Diam-98 <diamdiallo98@gmail.com>
2022-06-15 12:21:26 +0000

    Securite avance

--- src/main/java/diam/spring/springsecurity/Filters/JWTAuthenticationFilter.java
+++ src/main/java/diam/spring/springsecurity/Filters/JWTAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,7 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.springframework.security.config.Elements.JWT;
@@ -49,9 +52,19 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis()+5*60*1000))
                 .withIssuer(request.getRequestURL().toString())
-                .withClaim("roles", user.getAuthorities().stream().map(ga->ga.getAuthority()).collect(Collectors.toList()))
+//                .withClaim("roles", user.getAuthorities().stream().map(ga->ga.getAuthority()).collect(Collectors.toList()))
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
 
-        response.setHeader("Authorization", jwtAccessToken);
+        String jwtRefreshToken = com.auth0.jwt.JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+5*60*1000))
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+        Map<String, String> idToken = new HashMap<>();
+        idToken.put("access-token", jwtAccessToken);
+        idToken.put("access-token", jwtRefreshToken);
+//        response.setHeader("Authorization", jwtAccessToken);
+        response.setContentType("application/json");
     }
 }
